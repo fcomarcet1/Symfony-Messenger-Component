a88df32c4181df9748587897594b3dab5ac173f6
fcomarcet1 <fcomarcet1@gmail.com>
2021-10-13 19:33:21 +0200

    Add messenger.yaml config for async & command for send async message

--- src/Command/MessengerSendAsyncCommand.php
+++ src/Command/MessengerSendAsyncCommand.php
@@ -2,32 +2,30 @@
 
 namespace App\Command;
 
-use App\Messenger\Message\SyncMessage;
+use App\Messenger\Message\DefaultAsyncMessage;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Messenger\Bridge\Amqp\Transport\AmqpStamp;
 
-class MessengerSendSyncCommand extends Command
+class MessengerSendAsyncCommand extends Command
 {
-
-    protected static $defaultName = 'app:messenger:send-sync';
-    protected static $defaultDescription = 'Sends sync messages';
+    protected static $defaultName = 'app:messenger:send-async';
+    protected static $defaultDescription = 'Sends async messages';
     private MessageBusInterface $bus;
-    
+
     public function __construct(MessageBusInterface $bus)
     {
         $this->bus = $bus;
-        parent::__construct();   
     }
-
 
     protected function configure(): void
     {
         $this
-            ->setName('app:messenger:send-sync')
+            ->setName(self::$defaultName)
             ->setDescription(self::$defaultDescription)
             ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
             ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
@@ -36,11 +34,12 @@ class MessengerSendSyncCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        for ($i=0; $i <=10;) { 
-            $this->bus->dispatch(new SyncMessage(\sprintf('Data for message %s', $i)));
-            ++$i;
-        }
-      
+        // send message
+        $this->bus->dispatch(
+            new DefaultAsyncMessage('Default async message data'),
+            [new AmqpStamp('default_queue')]
+        );
+
         return Command::SUCCESS;
     }
 }
